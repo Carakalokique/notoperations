b087e6990ab7e214f633432cc33c808da98537d1
pbrosamond <pbrosamond@gmail.com>
2025-03-21 15:58:29 -0700

    chore: header styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,18 @@
-import './Hero.scss';
+import "./Hero.scss";
+import link from "../../assets/icons/link.png";
 
 function Hero() {
-    return (
-        <section className="hero">
-            {/* Add any hero content here */}
-        </section>
-    );
+  return (
+    <section className="hero">
+      <h1 className="hero__title">Are You in the Way or Helping Your Team?</h1>
+      <p className="hero__text">Take our 3-minute <span className="hero__text--emphasis">Operations Maturity Assessment</span> to know where you and your business stands.</p>
+      <p className="hero__text">
+        <a className="hero__link" href="#">Attend Workshop</a>
+        <img className="hero__link-icon" src={link} alt="Attend Workshop" />
+      </p>
+      <button className="hero__button">Take Assessment</button>
+    </section>
+  );
 }
 
 export default Hero;
