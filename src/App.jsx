64ebd2d077fee34e9624142553a30a6e74107e41
pbrosamond <pbrosamond@gmail.com>
2025-03-21 13:55:54 -0700

    Initial commit: React project setup with SASS and Prettier

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/main.scss'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Welcome to React</h1>
+      <p>This is a starter template with SASS and Prettier configured.</p>
+    </div>
+  )
+}
+
+export default App 
